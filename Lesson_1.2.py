1dbba9a25a638c8ae1ec1518c940fe2fdefc7cc2
Gorinov Maxim <39081283+gmaxm3@users.noreply.github.com>
2022-05-11 11:21:08 +0300

    Add files via upload

---
+++ Lesson_1.2.py
@@ -0,0 +1,21 @@
+#!python3
+
+# Assignment 01-01
+#
+#// Assign actual data to the variables below
+# and run the script to print the output.
+
+name = "Maxim"
+profession = "System Administrator"
+facility = "Laptop"
+city = "Dnepr"
+country = "Ukraina"
+goal = "software engineer";
+
+print(
+f"""
+My name is {name}. I am a {profession} at {facility} 
+in {city}, {country}. I want to learn how to program
+in Python to become a {goal}.
+"""
+) 
---
+++ Lesson_2.py
@@ -0,0 +1,6 @@
+import math
+r=float(input("Пожалуйста, введите радиус:"))
+h=float(input("Пожалуйста, введите высоту:"))
+pi=3.14
+V=pi*r*r*h
+print(f"Volume of cylinder with radius r={r} and height h={h}: {V}")
